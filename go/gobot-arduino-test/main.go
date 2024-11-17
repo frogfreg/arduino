37c0d1ff8c3a1d995d19460bccdd13667e58f6c2
frogfreg <bengallme@gmail.com>
2024-11-16 22:49:09 -0600

    servo function does make the servo move!

--- go/gobot-arduino-test/main.go
+++ go/gobot-arduino-test/main.go
@@ -10,6 +10,48 @@ import (
 )
 
 func main() {
+	servo()
+}
+
+func servo() {
+
+	firmataAdaptor := firmata.NewAdaptor("/dev/ttyUSB0")
+	sd := gpio.NewServoDriver(firmataAdaptor, "13")
+
+	work := func() {
+		for {
+			fmt.Println("setting to min...")
+			if err := sd.ToMin(); err != nil {
+				panic(err)
+			}
+			time.Sleep(1 * time.Second)
+			fmt.Println("setting to center...")
+			if err := sd.ToCenter(); err != nil {
+				panic(err)
+			}
+			time.Sleep(1 * time.Second)
+			fmt.Println("setting to max...")
+			if err := sd.ToMax(); err != nil {
+				panic(err)
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{sd},
+		work,
+	)
+
+	if err := robot.Start(); err != nil {
+		panic(err)
+	}
+
+}
+
+func led() {
+
 	firmataAdaptor := firmata.NewAdaptor("/dev/ttyUSB0")
 	led := gpio.NewLedDriver(firmataAdaptor, "13")
 
